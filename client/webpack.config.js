9ba271a49a1150bd494e05f52201cbd2bf3685e4
lumpenop <swk5373@gmail.com>
2022-08-09 01:11:15 +0900

    Add: webpack 설정 변경

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,43 +1,33 @@
-// webpack.config.js
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+// BundleAnalyzer는 Bundle 최적화 용도로 보통 저는 사용합니다.
 
 module.exports = {
-  entry: "./src/index.ts", // 번들링 시작 위치
-  output: {
-    path: path.join(__dirname, "/dist"), // 번들 결과물 위치
-    filename: "bundle.js",
-  },
+  entry: `${path.resolve(__dirname, "../client/src")}/index.tsx`,
   module: {
     rules: [
       {
-        test: /[\.js]$/, // .js 에 한하여 babel-loader를 이용하여 transpiling
-        exclude: /node_module/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.ts$/, // .ts 에 한하여 ts-loader를 이용하여 transpiling
-        exclude: /node_module/,
-        use: {
-          loader: "ts-loader",
-        },
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
     ],
   },
-  resolve: {
-    modules: [path.join(__dirname, "src"), "node_modules"], // 모듈 위치
-    extensions: [".ts", ".js"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // 템플릿 위치
+      template: `${path.resolve(__dirname, "../client/src/public")}/index.html`,
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
     }),
   ],
-  devServer: {
-    host: "localhost", // live-server host 및 port
-    port: 5500,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../client/src/"),
+    },
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
-  mode: "development", // 번들링 모드 development / production
+  mode: "development",
 };
