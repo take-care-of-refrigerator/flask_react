b87ebcee34d8882cd03a487dbb520c0432cd3054
lumpenop <swk5373@gmail.com>
2022-08-09 23:33:18 +0900

    Add: router 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Frige from "./components/Frige";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/frige" element={<Frige />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 export default App;
