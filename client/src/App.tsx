463ba4f43d253a8cc182c7fcf07401e44c44144b
lumpenop <swk5373@gmail.com>
2022-08-14 01:07:11 +0900

    Add: Styled-Components, Global Style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Frige from "./components/Frige";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
+import { ThemeProvider } from "styled-components";
+import theme from "./public/theme";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/frige" element={<Frige />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/frige" element={<Frige />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 export default App;
