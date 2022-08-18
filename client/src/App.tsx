d4f32587bcc1f87ad4366755f01798673aa4dbe5
lumpenop <swk5373@gmail.com>
2022-08-18 23:25:45 +0900

    Add: recoil 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,22 @@ import Home from "./components/Home";
 import Layout from "./components/Layout";
 import { ThemeProvider } from "styled-components";
 import theme from "./public/theme";
+import { RecoilRoot } from "recoil";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/frige" element={<Frige />} />
-          </Route>
-        </Routes>
-      </Router>
+      <RecoilRoot>
+        <Router>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/frige" element={<Frige />} />
+              <Route path="/diary/:id" element={<Frige />} />
+            </Route>
+          </Routes>
+        </Router>
+      </RecoilRoot>
     </ThemeProvider>
   );
 };
