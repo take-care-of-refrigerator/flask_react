9e1fc44765dc2d164f88df9b1eda77d6e18f7327
lumpenop <swk5373@gmail.com>
2022-08-18 23:26:03 +0900

    Add: date picker 클릭 시 라우팅 함수 추가

--- client/src/components/comp/Calendar.tsx
+++ client/src/components/comp/Calendar.tsx
@@ -1,16 +1,21 @@
 import DatePicker from "react-datepicker";
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
+import { useNavigate } from "react-router-dom";
 
 import styled from "styled-components";
 
 const Calendar = () => {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const navigate = useNavigate();
+  const dateChange = (date: Date | null) => {
+    navigate(`/diary/:${date?.toLocaleDateString()}`);
+  };
   return (
     <CalendarContainer>
       <DatePicker
         selected={startDate}
-        onChange={(date) => setStartDate(date)}
+        onChange={(date) => dateChange(date)}
         inline
       />
     </CalendarContainer>
