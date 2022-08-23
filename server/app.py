d42b0c1ff33e2c9affc321e8b097e447a729ff9b
lumpenop <swk5373@gmail.com>
2022-08-24 03:09:11 +0900

    Add: 커밋을 따로 할 여력이 없음

--- server/app.py
+++ server/app.py
@@ -6,8 +6,8 @@ CORS(app)
 
 @app.route('/', methods=('GET'))
 def index():
-  if request
-  return '<h1>Hello!</h1>'
+  if request.method == "GET":
+    return '<h1>Hello!</h1>'
 
 if __name__ == "__main__":
   app.run('0.0.0.0', debug=True)
