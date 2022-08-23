9f418bb86f03065ee187e5a9646455071ccafcaa
lumpenop <swk5373@gmail.com>
2022-08-24 03:29:21 +0900

    Add: 커밋을 따로 할 여력이 없음

--- client/src/routes/components/InputBox/SignIn.tsx
+++ client/src/routes/components/InputBox/SignIn.tsx
@@ -12,7 +12,7 @@ const SignIn = () => {
   useEffect(() => {}, [userIdRef]);
   return (
     <div>
-      <form action="http://localhost:5000" method="post">
+      <form action="http://10.0.15.202:5000" method="post">
         아이디:
         <input
           style={{margin: `0 10px`}}
