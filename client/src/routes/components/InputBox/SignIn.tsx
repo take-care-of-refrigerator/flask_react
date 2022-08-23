35da442a6283d9d11c2a5a3092bafc753665281c
lumpenop <swk5373@gmail.com>
2022-08-24 05:22:29 +0900

    Add: 커밋을 따로 할 여력이 없음

--- client/src/routes/components/InputBox/SignIn.tsx
+++ client/src/routes/components/InputBox/SignIn.tsx
@@ -14,7 +14,7 @@ const SignIn = () => {
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    axios.post('http://127.0.0.1:5000', {
+    axios.post('http://43.200.157.122:5000/', {
       userId,
       password
     })
