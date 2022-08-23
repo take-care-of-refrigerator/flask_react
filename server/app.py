88ffd92c2d5a6bc95a9c66ee0b832c7ac6e9d5cd
lumpenop <swk5373@gmail.com>
2022-08-24 03:10:18 +0900

    Add: 커밋을 따로 할 여력이 없음

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=('GET'))
+@app.route('/', methods=('GET', 'POST'))
 def index():
   if request.method == "GET":
     return '<h1>Hello!</h1>'
