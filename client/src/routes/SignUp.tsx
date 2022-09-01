30623b4dfd2683a7917f22c7e5e4ba4d584e3a86
lumpenop <swk5373@gmail.com>
2022-09-01 23:50:17 +0900

    Add: styled-components input 태그에 props로 값 전달 하였으나 추론이 안되고 있음

--- client/src/routes/SignUp.tsx
+++ client/src/routes/SignUp.tsx
@@ -1,14 +1,25 @@
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, ChangeEvent} from "react";
 
 import styled from "styled-components";
 
+interface InputProps {
+  readonly place: string;
+}
 
 
 const SignUp = () => {
   let userIdRef: React.MutableRefObject<HTMLInputElement | null> = useRef(null);
   const [userId, setUserId] = useState("");
   const [passowrd, setPassword] = useState("");
-  return (
+
+  const inputUserIdChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserId(event.currentTarget.value)
+  }
+
+  const inputPasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value)
+  }
+    return (
     <SignContainer>
       <form action="/">
         <SignBox>
@@ -17,7 +28,9 @@ const SignUp = () => {
         <SignUpInput
           type="text"
           value={userId}
-          onChange={(event) => setUserId(event.currentTarget.value)}
+          // @ts-ignore
+          place='아이디'
+          onChange={inputUserIdChange}
           ref={userIdRef}
         />
           </SignUpInputBox>
@@ -25,8 +38,10 @@ const SignUp = () => {
         비밀번호:
         <SignUpInput
           type="password"
+          // @ts-ignore
+          place='비밀번호'
           value={passowrd}
-          onChange={(event) => setPassword(event.currentTarget.value)}
+          onChange={inputPasswordChange}
         />
           </SignUpInputBox>
           <SignUpButton>회원가입</SignUpButton>
@@ -43,16 +58,20 @@ const SignContainer = styled.div`
   margin: 40px auto 0;
 `
 const SignBox = styled.div`
-  width: 35%;
+  width: 30%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
 `
 const SignUpInputBox = styled.div`
-  
+  width: 100%;
 `
-const SignUpInput = styled.input`
-  width: 80%;
+const SignUpInput = styled.input.attrs<InputProps>(
+  ({place}) => ({
+    placeholder: `${place}`
+  })
+)`
+  width: 50%;
 `
 
 const SignUpButton = styled.button`
